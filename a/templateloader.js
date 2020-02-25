ec8379213c07df7620477fa8f21c0aab2ce0f8d1
Arbira <mbl.acc4@gmail.com>
2020-02-25 13:24:55 +0300

    Update templateloader.js

--- a/templateloader.js
+++ a/templateloader.js
@@ -1,5 +1,17 @@
 
 // second template
+
+var dataprefix = document.currentScript.getAttribute('data-prefix');
+var templateprefix1 = document.currentScript.getAttribute('data-templateprefix1');  //about
+var templateprefix2 = document.currentScript.getAttribute('data-templateprefix2'); //events
+var prefix1 = "#";
+var prefix2 = "-template";
+var templatename1 = prefix1.concat(templateprefix1;prefix2); //#about-template
+var templatename2 = prefix1.concat(templateprefix2;prefix2); //#events-template
+
+console.log(templatename1, templatename2);
+
+
       var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=1L2Mr1hi_1D4o3aKcw3Pdb_CUdg5aDAU1XTPSKFiTHAI&output=html';
 
 

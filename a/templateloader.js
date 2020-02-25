4b8204829d244c1a2b85c4f5c48dda86d5268607
Arbira <mbl.acc4@gmail.com>
2020-02-25 13:53:48 +0300

    Update templateloader.js

--- a/templateloader.js
+++ a/templateloader.js
@@ -6,10 +6,13 @@ var templateprefix1 = document.currentScript.getAttribute('data-templateprefix1'
 var templateprefix2 = document.currentScript.getAttribute('data-templateprefix2'); //events
 var prefix1 = "#";
 var prefix2 = "-template";
+var sheetname1 = dataprefix.concat("-",templateprefix1); //mozhaysk-about
+var sheetname2 = dataprefix.concat("-",templateprefix2); //mozhaysk-events
+
 var templatename1 = prefix1.concat(templateprefix1,prefix2); //#about-template
 var templatename2 = prefix1.concat(templateprefix2,prefix2); //#events-template
 
-console.log(templatename1, templatename2);
+console.log(templatename1, templatename2, sheetname1, sheetname2 );
 
 
       var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=1L2Mr1hi_1D4o3aKcw3Pdb_CUdg5aDAU1XTPSKFiTHAI&output=html';

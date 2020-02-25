194535ea846cb89845c069d1b1b746ba0e62a2cf
Arbira <mbl.acc4@gmail.com>
2020-02-25 16:17:14 +0300

    Create menuloader.js

---
+++ a/menuloader.js
@@ -0,0 +1,22 @@
+
+// second template
+
+var datamenuitem = document.currentScript.getAttribute('data-menuitem');
+var datamenuitemorder = document.currentScript.getAttribute('data-menuitemorder');  //order
+var templateprefix2 = document.currentScript.getAttribute('data-templateprefix2'); //events
+
+
+//var templatename1 = prefix1.concat(templateprefix1,prefix2); //#about-template
+
+
+console.log(datamenuitem, datamenuitemorder);
+
+
+   
+  $(document).ready( function() {
+      
+      });
+
+
+	
+     
